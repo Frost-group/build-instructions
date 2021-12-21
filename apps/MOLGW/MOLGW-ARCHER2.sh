c5ab4527708f4943d023035501a5f56c60db19ce
Jarvist Moore Frost <jarvist@gmail.com>
2021-12-21 23:53:55 +0000

    GNU arb arith, and failing libint.

---
+++ apps/MOLGW/MOLGW-ARCHER2.sh
@@ -0,0 +1,43 @@
+module reset
+module load PrgEnv-gnu
+module load boost
+module load cray-python
+
+
+
+mkdir build
+cd build
+
+
+# GMP
+wget https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz 
+tar -xvf gmp-6.2.1.tar.xz
+
+pushd ./
+cd gmp-6.2.1
+./configure --prefix=${HOME}/opt/gmp-6.2.1/ --enable-cxx
+make -j
+make install
+popd
+
+# LIBINT
+wget https://github.com/evaleev/libint/archive/v2.6.0.tar.gz
+tar xzf v2.6.0.tar.gz
+
+pushd ./
+cd libint-2.6.0
+./autogen.sh
+mkdir build
+cd build
+CC=cc CXX=CC FC=ftn LDFLAGS=-dynamic ../configure --prefix=${HOME}/opt/libint-2.6.0/ \
+	--enable-1body=1 --enable-eri=0 --enable-eri3=0 --enable-eri2=0 --enable-contracted-ints \
+	--with-max-am=7 --with-opt-am=2 \
+	--with-cxxgen=g++ --with-cxxgen-optflags=-O1 --with-cxx=g++ --with-cxx-optflags=-O1
+make -j
+make install
+popd
+
+# LIBXC
+read p
+
+CC=cc CXX=CC FC=ftn ./configure --prefix=${HOME}/opt/libxc
