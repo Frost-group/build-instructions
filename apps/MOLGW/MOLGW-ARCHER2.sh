c419acb02d6ae4391812f1839b627ef26449ebd0
Jarvist Moore Frost <jarvist@gmail.com>
2021-12-22 00:03:00 +0000

    OK! Now failing on not finding BOOST.

--- apps/MOLGW/MOLGW-ARCHER2.sh
+++ apps/MOLGW/MOLGW-ARCHER2.sh
@@ -15,10 +15,13 @@ tar -xvf gmp-6.2.1.tar.xz
 
 pushd ./
 cd gmp-6.2.1
-./configure --prefix=${HOME}/opt/gmp-6.2.1/ --enable-cxx
+CC=cc CXX=CC FC=ftn LDFLAGS=-dynamic ./configure --prefix=${HOME}/opt/gmp-6.2.1/ --enable-cxx
 make -j
 make install
 popd
+
+export CPPFLAGS="-I${HOME}/opt/gmp-6.2.1/include"
+export LDFLAGS="-L${HOME}/opt/gmp-6.2.1/lib -dynamic"
 
 # LIBINT
 wget https://github.com/evaleev/libint/archive/v2.6.0.tar.gz
@@ -29,7 +32,7 @@ cd libint-2.6.0
 ./autogen.sh
 mkdir build
 cd build
-CC=cc CXX=CC FC=ftn LDFLAGS=-dynamic ../configure --prefix=${HOME}/opt/libint-2.6.0/ \
+CC=cc CXX=CC FC=ftn ../configure --prefix=${HOME}/opt/libint-2.6.0/ \
 	--enable-1body=1 --enable-eri=0 --enable-eri3=0 --enable-eri2=0 --enable-contracted-ints \
 	--with-max-am=7 --with-opt-am=2 \
 	--with-cxxgen=g++ --with-cxxgen-optflags=-O1 --with-cxx=g++ --with-cxx-optflags=-O1
