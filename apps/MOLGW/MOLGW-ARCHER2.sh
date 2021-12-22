4c5ec0ca0031f66d65556c185ba6efd6013b100a
Jarvist Moore Frost <jarvist@gmail.com>
2021-12-22 00:13:59 +0000

    OK! Configure passes; fails while building libint, linking GMP

--- apps/MOLGW/MOLGW-ARCHER2.sh
+++ apps/MOLGW/MOLGW-ARCHER2.sh
@@ -3,11 +3,8 @@ module load PrgEnv-gnu
 module load boost
 module load cray-python
 
-
-
 mkdir build
 cd build
-
 
 # GMP
 wget https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz 
@@ -35,7 +32,7 @@ cd build
 CC=cc CXX=CC FC=ftn ../configure --prefix=${HOME}/opt/libint-2.6.0/ \
 	--enable-1body=1 --enable-eri=0 --enable-eri3=0 --enable-eri2=0 --enable-contracted-ints \
 	--with-max-am=7 --with-opt-am=2 \
-	--with-cxxgen=g++ --with-cxxgen-optflags=-O1 --with-cxx=g++ --with-cxx-optflags=-O1
+	--with-cxxgen-optflags=-O1 --with-cxx-optflags=-O1
 make -j
 make install
 popd
